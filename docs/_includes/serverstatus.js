44d3f8bf4d13a76ae95e5250394e890544853b06
ProffFate <127612375+ProffFate@users.noreply.github.com>
2024-03-24 15:43:14 +0200

    Update serverstatus.js

--- docs/_includes/serverstatus.js
+++ docs/_includes/serverstatus.js
@@ -9,7 +9,7 @@ function handleErrors(response) {
 }
 
 // Fetch API, mcsrvstat
-fetch("https://api.mcsrvstat.us/{% if site.useBedrockForOnlineStatus == true %}bedrock/{% endif %}3/{{ site.serverIP }}")
+fetch("https://api.mcstatus.io/{% if site.useBedrockForOnlineStatus == true %}bedrock/{% endif %}v2/{{ site.serverIP }}")
     .then(handleErrors)
     .then(response => {
         return response.json();
