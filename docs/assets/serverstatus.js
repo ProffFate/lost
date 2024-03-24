40734730e14b9e40e6c07e91cbbe8a3eff6d7154
ProffFate <127612375+ProffFate@users.noreply.github.com>
2024-03-24 15:39:04 +0200

    Update serverstatus.js

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -5,7 +5,7 @@
     if (data.online == true) {
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Почати Гру: ${data.players.online} ${data.players.list.name} Онлайн</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Почати Гру: ${data.players.online} Онлайн</span>`;
         {% else %}
         document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Ми онлпйн!&ensp;&middot;&ensp;${data.players.online} грає</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp; Онлайн, ${data.players.online} playing</p>`;
